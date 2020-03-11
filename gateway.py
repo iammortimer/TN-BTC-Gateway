2b3da816ad8808166ee50f6d54078235721b02c2
iammortimer <info@mullerbeheer.nl>
2020-03-11 11:03:44 +0100

    update used config

--- gateway.py
+++ gateway.py
@@ -16,7 +16,7 @@ security = HTTPBasic()
 app.mount("/static", StaticFiles(directory="static"), name="static")
 templates = Jinja2Templates(directory="templates")
 
-with open('config_run.json') as json_file:
+with open('config.json') as json_file:
     config = json.load(json_file)
 
 instance = authproxy.AuthServiceProxy(config['other']['node'])
