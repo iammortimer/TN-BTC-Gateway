afdcf99577a4809b2805f59422ebeaff0d394669
iammortimer <info@mullerbeheer.nl>
2020-03-21 21:33:30 +0100

    type change in fullinfo api

--- gateway.py
+++ gateway.py
@@ -177,7 +177,7 @@ async def api_fullinfo(request: Request):
             "other_balance": otherBalance,
             "minAmount": config['main']['min'],
             "maxAmount": config['main']['max'],
-            "type": "tunnel"}
+            "type": "deposit"}
 
 @app.get("/api/deposit/{tnAddress}")
 async def api_depositCheck(tnAddress):
